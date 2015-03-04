066252bf43d266950fb955611ce60c7bcf6826e2
Cruz Julian Bishop <cruzjbishop@gmail.com>
2015-03-04 22:27:29 +1000

    Add a logger

--- src/main/java/com/carpentersblocks/CarpentersBlocks.java
+++ src/main/java/com/carpentersblocks/CarpentersBlocks.java
@@ -1,6 +1,8 @@
 package com.carpentersblocks;
 
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * CarpentersBlocks - A utility class that defines the actual {@link net.minecraftforge.fml.common.Mod}
@@ -17,4 +19,9 @@ public class CarpentersBlocks {
      * The {@link java.lang.String} containing the version of this {@link net.minecraftforge.fml.common.Mod}
      */
     public static final String MOD_VERSION = "4.0.0-dev";
+
+    /**
+     * The {@link org.apache.logging.log4j.Logger} for this {@link net.minecraftforge.fml.common.Mod}
+     */
+    public static final Logger LOGGER = LogManager.getLogger("CarpentersBlocks");
 }
