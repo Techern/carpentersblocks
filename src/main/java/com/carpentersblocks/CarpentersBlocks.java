f65a1f88d0647d2ce367c6382e3967a211595920
Cruz Julian Bishop <cruzjbishop@gmail.com>
2015-03-04 21:56:48 +1000

    Initial CarpentersBlocks class

---
+++ src/main/java/com/carpentersblocks/CarpentersBlocks.java
@@ -0,0 +1,20 @@
+package com.carpentersblocks;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * CarpentersBlocks - A utility class that defines the actual {@link net.minecraftforge.fml.common.Mod}
+ */
+@Mod(modid = CarpentersBlocks.MOD_ID, version = CarpentersBlocks.MOD_VERSION)
+public class CarpentersBlocks {
+
+    /**
+     * The {@link java.lang.String} containing the ID of this {@link net.minecraftforge.fml.common.Mod}
+     */
+    public static final String MOD_ID = "CarpentersBlocks";
+
+    /**
+     * The {@link java.lang.String} containing the version of this {@link net.minecraftforge.fml.common.Mod}
+     */
+    public static final String MOD_VERSION = "4.0.0-dev";
+}
